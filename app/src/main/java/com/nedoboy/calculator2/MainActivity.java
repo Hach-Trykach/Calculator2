8355452186eefe14ea9e5db788a228d0813e60c6
Hach-Trykach <dudarev.vlad@gmail.com>
2020-10-23 21:13:10 +0300

    Update MainActivity.java

--- app/src/main/java/com/nedoboy/calculator2/MainActivity.java
+++ app/src/main/java/com/nedoboy/calculator2/MainActivity.java
@@ -2,21 +2,16 @@ package com.nedoboy.calculator2;
 
 import android.app.Activity;
 import android.os.Bundle;
-//import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-//import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
     final int MENU_RESET_ID = 1;
     final int MENU_QUIT_ID = 2;
-
-// EditText etNum1;
-// EditText etNum2;
 
     Button btnAdd, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0, btnSin, btnResult;
     Button btnSub;
@@ -27,19 +22,14 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     String oper = "", pole = "";
 
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-// находим элементы
-// etNum1 = (EditText) findViewById(R.id.etNum1);
-// etNum2 = (EditText) findViewById(R.id.etNum2);
-
-        btnAdd = (Button) findViewById(R.id.btnAdd);
-        btnSub = (Button) findViewById(R.id.btnSub);
-        btnMult = (Button) findViewById(R.id.btnMult);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnSub = findViewById(R.id.btnSub);
+        btnMult = findViewById(R.id.btnMult);
         btnDiv = findViewById(R.id.btnDiv);
         btnSin = findViewById(R.id.btnSin);
 
@@ -79,19 +69,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
 // TODO Auto-generated method stub
-// float num1 = 0;
-// float num2 = 0;
         float result = 0;
-
-// Проверяем поля на пустоту
-// if (TextUtils.isEmpty(etNum1.getText().toString())
-// || TextUtils.isEmpty(etNum2.getText().toString())) {
-// return;
-// }
-
-// читаем EditText и заполняем переменные числами
-// num1 = Float.parseFloat(etNum1.getText().toString());
-// num2 = Float.parseFloat(etNum2.getText().toString());
 
 // определяем нажатую кнопку и выполняем соответствующую операцию
 // в oper пишем операцию, потом будем использовать в выводе
@@ -204,8 +182,6 @@ public class MainActivity extends Activity implements View.OnClickListener {
         switch (item.getItemId()) {
             case MENU_RESET_ID:
 // очищаем поля
-// etNum1.setText("");
-// etNum2.setText("");
                 tvResult.setText("");
                 break;
             case MENU_QUIT_ID:
